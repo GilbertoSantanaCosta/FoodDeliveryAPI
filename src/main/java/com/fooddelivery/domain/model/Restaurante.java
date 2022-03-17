48a163770e9dc7d323ba47effa2b884e2af8f1fb
GilbertoSantanaCosta <you@example.comg.s.d.c@hotmail.comgit config --global user.name Your>
2022-03-17 07:44:51 -0300

    JoinColumn

--- src/main/java/com/fooddelivery/domain/model/Restaurante.java
+++ src/main/java/com/fooddelivery/domain/model/Restaurante.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import lombok.Data;
@@ -26,6 +27,7 @@ public class Restaurante implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
+	//@JoinColumn(name = "taxa_frete")
 	private BigDecimal taxaFrete;
 	
 	@ManyToOne
