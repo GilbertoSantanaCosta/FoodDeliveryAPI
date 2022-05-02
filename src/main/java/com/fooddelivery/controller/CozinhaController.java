9dab4162c0cba136766d01a400c59bd012d28da1
GilbertoSantanaCosta <you@example.comg.s.d.c@hotmail.comgit config --global user.name Your>
2022-05-02 07:09:57 -0300

    Modelando e implementando a exclusão de recursos com DELETE

--- src/main/java/com/fooddelivery/controller/CozinhaController.java
+++ src/main/java/com/fooddelivery/controller/CozinhaController.java
@@ -1,12 +1,13 @@
 package com.fooddelivery.controller;
 
-
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,54 +21,72 @@ import com.fooddelivery.api.model.CozinhaXmlWrapper;
 import com.fooddelivery.domain.jpa.cozinha.CozinhaRepository;
 import com.fooddelivery.domain.model.Cozinha;
 
-
 @RestController
 @RequestMapping("/cozinhas")
 public class CozinhaController {
 
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
-	
+
 	@GetMapping
-	public List<Cozinha> listar(){
-		
+	public List<Cozinha> listar() {
+
 		return cozinhaRepository.Listar();
 	}
-	
-	
+
 	@GetMapping(produces = org.springframework.http.MediaType.APPLICATION_XML_VALUE)
-	public CozinhaXmlWrapper listarWrapperXml(){
-		
+	public CozinhaXmlWrapper listarWrapperXml() {
+
 		return new CozinhaXmlWrapper(cozinhaRepository.Listar());
 	}
-	
+
 	@GetMapping("/{cozinhaId}")
 	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
-		
+
 		Cozinha c = cozinhaRepository.buscar(cozinhaId);
-		
-		if(c != null) {
-			
+
+		if (c != null) {
+
 			return ResponseEntity.ok().body(c);
 		}
-		
+
 		return ResponseEntity.notFound().build();
- 		
+
 	}
-	
+
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public void adicionar(@RequestBody Cozinha cozinha) {
-		
+
 		cozinhaRepository.salvar(cozinha);
 	}
-	
+
 	@PutMapping("/{cozinhaId}")
-	public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId){
-		return null;
+	public ResponseEntity<Cozinha> atualizar(@PathVariable Cozinha cozinha) {
+
+		Cozinha c = cozinhaRepository.buscar(cozinha.getId());
+
+		if (c != null) {
+			BeanUtils.copyProperties(c, cozinha, "id");
+
+			cozinhaRepository.salvar(c);
+			
+			return ResponseEntity.ok(c);
+		}
+
+		return ResponseEntity.notFound().build();
 	}
-	
-	// teste
-	
-	
+
+	@DeleteMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> remover(@PathVariable Long cozinhaId) {
+		Cozinha c = cozinhaRepository.buscar(cozinhaId);
+
+		if (c != null) {
+			cozinhaRepository.remover(cozinhaId);
+
+			return ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.notFound().build();
+	}
+
 }
